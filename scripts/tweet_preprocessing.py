0524af1ee26a3f96a35229f75803e0bfb99dda73
sarahondraszek <s1saondr@uni-trier.de>
2021-05-04 14:12:23 +0200

    Tweet preprocessing

---
+++ scripts/tweet_preprocessing.py
@@ -0,0 +1,20 @@
+import spacy
+import spacy.cli
+import getting_tweets_v2
+import re
+
+""" Spacy loading """
+spacy.cli.download('de_core_news_md')
+nlp_ge = spacy.load('de_core_news_md')
+
+""" Make files -> preparation for further processing """
+
+getting_tweets_v2.collect_tweets_in_files()
+tweet_file = open('tweets_2.txt', 'r').read()
+
+
+""" Remove emoticons, usernames, retweets-definition -> plain text """
+
+
+
+""" Make spacy doc for text processing """
