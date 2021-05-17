c262fbf1f5bdb0e2190696734491137ca7615f3c
sarahondraszek <s1saondr@uni-trier.de>
2021-05-17 19:55:44 +0200

    changed script

---
+++ scripts/preprocessing_tweets.py
@@ -0,0 +1,86 @@
+import glob
+from nltk.util import pr
+import pandas as pd
+import re
+import nltk
+import spacy
+import spacy.cli
+from nltk.corpus import stopwords
+from gensim.corpora import Dictionary
+import time
+
+""" Time measurements """
+
+start = time.process_time()
+
+""" Spacy installments """
+
+# spacy.cli.download('de_core_news_md')
+nlp = spacy.load('de_core_news_md')
+nlp.max_length = 2000000
+
+
+def preprocess_data(file_list):
+    docs = []
+    for file in file_list:
+        dataframe = pd.read_csv(file, usecols=['text'], lineterminator='\n')  # Datei öffnen
+        datastring = dataframe.to_string(index=False)  # String erstellen aus Dataframe
+        datastring = re.sub(r"@[\S]*|#", "", datastring)  # Nutzernamen und Hashtag-Zeichen entfernen
+        datastring = re.sub("https:[\S]*|www[\S]*", "", datastring)  # Links entfernen
+        datastring = re.sub(r"[0-9]*", "", datastring)  # Zahlen entfernen
+        datastring = re.sub(r"\\n", " ", datastring)  # Zeilenumbruchmarker entfernen
+        datastring = re.sub("\W", " ", datastring)  # Satzzeichen entfernen
+        datastring = re.sub("RT|NaN|via", "", datastring)  # Entfernen von twitterspezifiscehen Markern
+
+        token = nltk.wordpunct_tokenize(datastring.lower())  # Tokeniserung + nur Kleinschreibung
+
+        stopwords_ger = nltk.corpus.stopwords.words("german")  # Liste der Stoppwörter
+        removed_stopwords = [w for w in token if not w in stopwords_ger]  # Stoppwörter entfernen
+        text = [w for w in removed_stopwords if len(w) > 2]  # Wörter < 2 Buchstaben entfernen
+
+        list = []  # Liste der Token wird erzeugt und der Liste docs hinzugefügt
+        for w in text:
+            list.append(w)
+        docs.append(list)
+
+    return docs
+
+
+""" Create docs from files """
+
+file_list = glob.glob('./corpus/*.csv')
+docs = preprocess_data(file_list)
+
+""" Lemmatizing words """
+
+
+def lemmatizer(docs):
+    lemmatized_words = []
+    for word_list in docs:
+        temp_list = []
+        temp_doc = nlp(' '.join(word_list))
+        for word in temp_doc:
+            temp_list.append(word.lemma_)
+        lemmatized_words.append(temp_list)
+    return lemmatized_words
+
+
+docs = lemmatizer(docs=docs)
+
+
+""" Make gensim.Dictionary and filter extremes """
+
+dictionary = Dictionary(docs)
+dictionary.filter_extremes(no_below=3, no_above=0.5)
+
+
+""" BOW representation """
+
+corpus = [dictionary.doc2bow(doc) for doc in docs]
+
+print('Number of unique tokens: %d' % len(dictionary))
+print('Number of documents: %d' % len(corpus))
+
+print(time.process_time() - start)
+
+
